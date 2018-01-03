923b01df37dd07214f32d4696025148138cf8794
Nikita <shockck84@gmail.com>
2018-01-03 15:38:30 +0500

    Update loader files

--- lua/autorun/client/mt_cl_loader.lua
+++ lua/autorun/client/mt_cl_loader.lua
@@ -1,2 +1,5 @@
 -- This file loads all clientside scripts.
 include('mt/client/machine_menu_handler.lua')
+include('mt/client/localization.lua')
+include('mt/client/spawner_descriptions.lua')
+include('mt/client/convars.lua')
--- lua/autorun/server/mt_sv_loader.lua
+++ lua/autorun/server/mt_sv_loader.lua
@@ -1,2 +1,5 @@
 -- This file loads all serverside scripts.
 AddCSLuaFile('mt/client/machine_menu_handler.lua')
+AddCSLuaFile('mt/client/localization.lua')
+AddCSLuaFile('mt/client/convars.lua')
+AddCSLuaFile('mt/client/spawner_descriptions.lua')
