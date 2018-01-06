83409273dfd7c3e272a4235cf9a299e38f624bcd
Nikita <shockck84@gmail.com>
2018-01-06 12:36:36 +0500

    Worked on water pump

--- lua/entities/mt_water_pump/cl_init.lua
+++ lua/entities/mt_water_pump/cl_init.lua
@@ -1,15 +1,16 @@
 include('shared.lua')
 
 function ENT:drawSetupFrame()
-    local frame = vgui.Create('DFrame')
-    frame:SetSize(500, 500)
-    frame:SetTitle('Water pump')
-    frame:SetPos(ScrW() * 0.3, ScrH() * 0.3)
+    local frame = self:createSetupFrame()
 
-    local powerButton = vgui.Create('DButton', frame)
-    powerButton:SetPos(10, 30)
-    powerButton:SetSize(80, 80)
-    powerButton:SetText('Power')
+    local waterOutputLabel = vgui.Create('DLabel')
+    local outputLevel
+    if self:isPumpingWater() then
+        outputLevel = self:GetPumpingRatio()
+    else
+        outputLevel = 0.0
+    end
+    waterOutputLabel:SetText(outputLevel .. ' L/s')
 
     frame:MakePopup()
 end
--- lua/entities/mt_water_pump/shared.lua
+++ lua/entities/mt_water_pump/shared.lua
@@ -2,3 +2,12 @@ ENT.Type = 'anim'
 ENT.Base = 'mt_machine_base'
 
 ENT.PrintName = 'Water Pump'
+
+function ENT:SetupDataTables()
+	self:machineSetupDataTables()
+    self:NetworkVar('Float', 0, 'PumpingRatio')
+end
+
+function ENT:isPumpingWater()
+    return self:GetEnabled() and self:WaterLevel() ~= 0
+end
