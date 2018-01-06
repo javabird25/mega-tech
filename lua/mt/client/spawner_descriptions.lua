91338c8932c451a389a1f60fcbfe8bdd36115d3c
Nikita <shockck84@gmail.com>
2018-01-06 12:37:00 +0500

    Worked on spawner descriptions

--- lua/mt/client/spawner_descriptions.lua
+++ lua/mt/client/spawner_descriptions.lua
@@ -2,9 +2,9 @@ MT_spawnerDescriptions = {
     ['Tier 2 tech'] = {
         ['Water pump'] = {
             class = 'mt_water_pump',
-            description = [[Gets water from water sources and transmits it to connected pipes.;
+            description = [[Gets water from water sources and transmits it to connected pipes.
 
-Water output: 6 L/s
+Water output volume: 6 L/s
 HP: 200]]
         },
 
@@ -12,9 +12,19 @@ HP: 200]]
             class = 'mt_pipe',
             description = [[Transports liquids.
 
-Maximal transmit volume: 2 L/s
+Maximal transmit volume: 6 L/s
 HP: 100]]
         }
     }
 
 }
+
+function getNameOfClass(class)
+    for _, category in pairs(MT_spawnerDescriptions) do
+        for name, config in pairs(category) do
+            if config.class == class then
+                return name
+            end
+        end
+    end
+end
